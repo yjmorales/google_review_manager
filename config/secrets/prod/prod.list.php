74bc0fe477b7eff981f1d83c0869f5d3d8cbed15
yjimenez <yjimenez@sonnysdirect.com>
2023-01-05 16:00:45 -0500

    Prod secrets

---
+++ config/secrets/prod/prod.list.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'GOOGLE_MAP_API_KEY' => null,
+    'REDIS_HOST' => null,
+    'REDIS_PASSWORD' => null,
+    'REDIS_PORT' => null,
+    'SENDGRID_API_KEY' => null,
+    'SENDGRID_INTERNAL_TO_EMAIL' => null,
+    'SENDGRID_SENDER_EMAIL' => null,
+    'SYS_ADMIN_EMAIL' => null,
+    'SYS_ADMIN_INDEED_PROFILE' => null,
+    'SYS_ADMIN_LINKEDIN_PROFILE_URL' => null,
+    'SYS_ADMIN_PERSONAL_PAGE_URL' => null,
+    'SYS_ADMIN_PHONE' => null,
+];
